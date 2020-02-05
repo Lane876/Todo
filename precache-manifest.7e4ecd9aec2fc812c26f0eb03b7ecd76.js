b77f10b88526894067813caf97a189fb5d38bded
lane876 <milenkovicmilan10@gmail.com>
2020-02-05 21:52:37 +0100

    Updates

---
+++ precache-manifest.7e4ecd9aec2fc812c26f0eb03b7ecd76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd8dbdd2febe72620b190687c0dfebd",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "7f7ec15a162df065b5e3",
+    "url": "/Todo/static/css/main.b76e332d.chunk.css"
+  },
+  {
+    "revision": "6a6cb1235700bdada0ab",
+    "url": "/Todo/static/js/2.e724de16.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Todo/static/js/2.e724de16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7ec15a162df065b5e3",
+    "url": "/Todo/static/js/main.07b30962.chunk.js"
+  },
+  {
+    "revision": "c620354e03c283f6f624",
+    "url": "/Todo/static/js/runtime-main.ed977463.js"
+  }
+]);
